49c48ec9651accbc59ca5c68fcd14490fbc1a14b
rodriguesfortes6711-ux <rodriguesfortes6711@gmail.com>
2026-06-24 21:42:33 -0300

    Add files via upload

--- db.js
+++ db.js
@@ -1,11 +1,10 @@
-// db.js
 const { Pool } = require('pg');
 require('dotenv').config();
 
 const pool = new Pool({
     connectionString: process.env.DB_URL,
     ssl: {
-        rejectUnauthorized: false // Necessário para conexões externas (Neon, Supabase, etc.)
+        rejectUnauthorized: false
     }
 });
 
